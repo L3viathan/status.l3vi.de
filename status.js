74c2f45f900dbbc3f1838783f106689b7540dee7
L3viathan <git@l3vi.de>
2014-09-17 01:20:44 +0200

    initial commit

---
+++ status.js
@@ -0,0 +1,79 @@
+var l3viAPI = "http://api.l3vi.de/";
+var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?";
+var timezoneAPI = "https://maps.googleapis.com/maps/api/timezone/json";
+
+var offset = 0;
+var intervals = {};
+
+function toFixed(n) {
+	return n<10?"0"+n:n;	
+}
+
+function refreshTime() {
+	var offset = +$("#time").data("offset");
+	var x = new Date(+new Date() + offset);
+	setText("#time",toFixed(x.getUTCHours()) + ":" + toFixed(x.getMinutes()) + ":" + toFixed(x.getSeconds()));
+	setTimeout(refreshTime, 1000);
+}
+
+function getData() {
+	var location = $.ajax({
+		dataType: "json",
+		url: l3viAPI + "location.json",
+		async: false
+	}).responseJSON;
+	
+	setText("#address",location["address"]);
+	
+	$.getJSON(l3viAPI + "status.json", function(data){
+		setText("#status",data["status"]);
+	});
+	
+	$.getJSON(l3viAPI + "mood.json", function(data){
+		setText("#mood",data["mood"]);
+	});
+	
+	$.getJSON(weatherAPI, {lat: location.lat, lon: location.lon},function(data){
+		setText("#city",data["name"]);
+		setText("#temp",Math.round(10*(data["main"]["temp"]-272.15))/10 + "°C");
+		setText("#weather",data["weather"][0]["description"]);
+	});
+	
+	$.getJSON(timezoneAPI + "?location=" + location.lat + "," + location.lon + "&timestamp=" + +new Date()/1000, function(data){
+		$("#time").data("offset",(data["rawOffset"] + data["dstOffset"]) * 1000);
+		refreshTime();
+	});
+}
+
+function setText(where,what) {
+	clearInterval(intervals[where]);
+	$(where).text(what);
+}
+
+function jibber(what) {
+	var len=$(what).text().length;
+	var ret="";
+	for(var i=len;i>0;i--){
+		ret += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[Math.round(Math.random()*61)];
+	}
+	$(what).text(ret);
+}
+
+function jibbertime() {
+	$("#time").text(Math.round(Math.random()*100) + ":" + Math.round(Math.random()*100) + ":" + Math.round(Math.random()*100));
+}
+
+function jibbertemp() {
+	$("#temp").text(Math.round(Math.random()*100)/10 + "°C");
+}
+
+$(function(){
+	intervals["#status"] = setInterval("jibber('#status')", 100);
+	intervals["#address"] = setInterval("jibber('#address')", 100);
+	intervals["#mood"] = setInterval("jibber('#mood')", 100);
+	intervals["#time"] = setInterval("jibbertime()", 100);
+	intervals["#city"] = setInterval("jibber('#city')", 100);
+	intervals["#temp"] = setInterval("jibbertemp()", 100);
+	intervals["#weather"] = setInterval("jibber('#weather')", 100);
+	setTimeout(getData,1000);
+});
